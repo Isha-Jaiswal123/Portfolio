7e403525b39dd92117d452b287148e0375efa043
Isha-Jaiswal123 <ishujaiswal746@gmail.com>
2024-01-08 19:10:10 +0530

    Add Inisial files

---
+++ index.js
@@ -0,0 +1,11 @@
+function run(){
+     const bars=document.getElementById('bars');
+     const menu=document.getElementById('menu');
+     if(menu.style.display !="none"){
+        menu.style.display ="none";
+     }
+     else{
+        menu.style.display ="block";
+     }
+}
+
